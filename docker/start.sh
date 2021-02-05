888e39659f99e200d2b3079b10d0d33467d4de52
Carey P Gumaer <gumaerc@mit.edu>
2021-02-05 09:22:05 -0500

    docker configuration (#30)
    * first draft of working docker config
    
    * example env
    
    * remove the example course id from build args and source it from env so you don't need to rebuild the container to switch courses
    
    * mount the theme directory instead of copying in
    
    * mount path to `ocw-to-hugo` externally instead of cloning internally, but allow env variables to override input folder and download functionality
    
    * add pdfjs install
    
    * delete errant log file
    
    * support using local content instead of ocw-to-hugo and consolidate start script
    
    * readme updates
    
    * OCW_TO_HUGO_DOWNLOAD true by default
    
    * clarify usage of OCW_TO_HUGO_INPUT
    
    * default OCW_TO_HUGO_INPUT to /ocw-data/input
    
    * add OCW_TEST_COURSE description to the readme
    
    * don't need backticks here
    
    * rename service to theme for brevity
    
    * add pipefail line and separate out cd theme from npm start
    
    * remap AWS_ACCESS_KEY_ID to AWS_ACCESS_KEY for ocw-to-hugo for now

---
+++ docker/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Starting services..."
+set -e -o pipefail
+cd /theme
+npm start &
+export PATH=$PATH:/usr/local/go/bin
+# If the path to ocw-to-hugo is not set, use local content
+# Otherwise, download course content and use that instead
+if [[ -z "${OCW_TO_HUGO_PATH}" ]]; then
+  cd /theme/docker/hugo && /usr/local/bin/hugo server --bind 0.0.0.0
+else
+  cd /ocw-to-hugo && yarn install && cd /theme && yarn install
+  rm -rf /ocw-data/* && mkdir /ocw-data/input && mkdir /ocw-data/output
+  echo "{\"courses\":[\"${OCW_TEST_COURSE}\"]}" | tee /ocw-data/courses.json
+  cd /ocw-to-hugo && node . -i ${OCW_TO_HUGO_INPUT:-/ocw-data/input} \
+    -o /ocw-data/output -c /ocw-data/courses.json --download ${OCW_TO_HUGO_DOWNLOAD:-true}
+  rm -rf /ocw-to-hugo-output/* && mkdir /ocw-to-hugo-output/content && mkdir /ocw-to-hugo-output/data
+  cp /theme/docker/hugo/go.mod /ocw-to-hugo-output/go.mod
+  cp /theme/docker/hugo/config.toml /ocw-to-hugo-output/config.toml
+  cp -R /ocw-data/output/content/courses/${OCW_TEST_COURSE}/* /ocw-to-hugo-output/content/
+  cp /ocw-data/output/data/courses/${OCW_TEST_COURSE}.json /ocw-to-hugo-output/data/course.json
+  cd /ocw-to-hugo-output && /usr/local/bin/hugo server --bind 0.0.0.0
+fi
