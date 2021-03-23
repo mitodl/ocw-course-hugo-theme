9e07299e67970f3bd346096c8c727b7e23379e9e
Carey Gumaer <gumaerc@mit.edu>
2021-03-23 17:09:02 -0400

    update docker config to support config folder

--- docker/start.sh
+++ docker/start.sh
@@ -15,9 +15,9 @@ else
   echo "{\"courses\":[\"${OCW_TEST_COURSE}\"]}" | tee /ocw-data/courses.json
   cd /ocw-to-hugo && node . -i ${OCW_TO_HUGO_INPUT:-/ocw-data/input} \
     -o /ocw-data/output -c /ocw-data/courses.json --download ${OCW_TO_HUGO_DOWNLOAD:-true}
-  rm -rf /ocw-to-hugo-output/*
+  rm -rf /ocw-to-hugo-output/* && mkdir -p /ocw-to-hugo-output/config/_default
   cp /theme/docker/hugo/go.mod /ocw-to-hugo-output/go.mod
-  cp /theme/docker/hugo/config.toml /ocw-to-hugo-output/config.toml
+  cp /theme/docker/hugo/config/_default/config.toml /ocw-to-hugo-output/config/_default/config.toml
   cp -R /ocw-data/output/${OCW_TEST_COURSE}/* /ocw-to-hugo-output/
   cd /ocw-to-hugo-output && /usr/local/bin/hugo server --bind 0.0.0.0
 fi
