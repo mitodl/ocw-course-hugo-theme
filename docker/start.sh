ec0bd7d00363e28fe9ccc3acf3c13d48e11bcac9
Carey P Gumaer <gumaerc@mit.edu>
2021-03-26 11:40:03 -0400

    update docker config to support config folder (#65)
    * update docker config to support config folder
    
    * separate output directory clear command and config directory creation command

--- docker/start.sh
+++ docker/start.sh
@@ -16,8 +16,9 @@ else
   cd /ocw-to-hugo && node . -i ${OCW_TO_HUGO_INPUT:-/ocw-data/input} \
     -o /ocw-data/output -c /ocw-data/courses.json --download ${OCW_TO_HUGO_DOWNLOAD:-true}
   rm -rf /ocw-to-hugo-output/*
+  mkdir -p /ocw-to-hugo-output/config/_default
   cp /theme/docker/hugo/go.mod /ocw-to-hugo-output/go.mod
-  cp /theme/docker/hugo/config.toml /ocw-to-hugo-output/config.toml
+  cp /theme/docker/hugo/config/_default/config.toml /ocw-to-hugo-output/config/_default/config.toml
   cp -R /ocw-data/output/${OCW_TEST_COURSE}/* /ocw-to-hugo-output/
   cd /ocw-to-hugo-output && /usr/local/bin/hugo server --bind 0.0.0.0
 fi
